70a419fb2cd58d5b6f734b0ad1eebe74a9a6a211
antosha.4e@gmail.com <antosha.4e@gmail.com@80208412-e658-0229-2742-08e265f7ad83>
2014-02-09 16:39:34 +0000

    ...

--- trunk/src/com/payday/A.java
+++
@@ -1,8 +0,0 @@
-package com.payday;
-
-/**
- * User: antosha
- * Date: 1/20/14
- */
-public class A {
-}
---
+++ trunk/src/com/payday/activity/TabbedActivity.java
@@ -0,0 +1,25 @@
+package com.payday.activity;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.TabActivity;
+import android.os.Bundle;
+import android.widget.TabHost;
+import com.example.PayDay.R;
+
+public class TabbedActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_test);
+
+        final ActionBar bar = getActionBar();
+        bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        bar.setDisplayOptions(0, ActionBar.DISPLAY_SHOW_TITLE);
+
+//        bar.addTab(bar.newTab()
+//                .setText("Simple")
+//                .setTabListener(new TabListener<FragmentStack.CountingFragment>(
+//                        this, "simple", FragmentStack.CountingFragment.class)));
+    }
+}
